e26c491e012418bdfba2a9cf8fe4c4346a4c3b2e
kurakinatvl1 <kurakinatvl1@gmail.com>
2026-07-04 15:10:28 +0200

    Add files via upload

---
+++ 4th 27.06.26.py
@@ -0,0 +1,72 @@
+# ph = input()
+# # a = float(input())
+# # pi = 3.14
+# # if ph == 'круг' :
+# #     print(a**2*pi)
+# # else :
+# #     print(a**2)
+
+
+# n = int(input())
+# a = n // 1000
+# b =(n // 100) % 10
+# c = (n // 10) % 10
+# d = n % 10
+# print(a, b, c, d)
+# if a+b == c+d:
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# a1, b1 = 1 , 5
+# a2 , b2 = 3 , 8
+# if a1>a2:
+#     left = a1
+# else:
+#     left = a2
+#
+# if b1<b2:
+#     right = b1
+# else:
+#     right = b2
+#
+# if left<right:
+#     print('пересекаются в:', left,right)
+# elif left == right:
+#     print('пересекаются в точке', left)
+# else:print('не пересекаются')
+
+
+# m = str(input())
+# n = int(input())
+# if (input == 'jan' or 'mar' or 'may' or 'jul' or 'aug' or 'oct'or 'dec') and 1<n<=31:
+#     print('correct')
+# elif 1<n<=30 and (input == 'apr' or 'jun' or 'sep' or 'nov'):
+#      print('correct')
+# elif 1<n<=28 and (input == 'feb' ):
+#      print('correct')
+# else:
+#     print('error')
+
+# n1 = 1
+# n2 = 2
+# match n2:
+#     case 4,6,9,11:
+#         if 1<n1<=30:
+#           print('correct')
+#         else:
+#             print('error')
+#     case 1, 3 , 5 , 7 , 8 , 10 ,12:
+#         if 1 < n1 <= 30:
+#          print('correct')
+#         else:
+#          print('error')
+#     case 2:
+#         if 1 < n1 <= 30:
+#          print('correct')
+#         else:
+#          print('error')
+#     case _:
+#          print('error')
+
