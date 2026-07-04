d14d3095b3afd6b6e17a0840b7ba6bf6e667e514
kurakinatvl1 <kurakinatvl1@gmail.com>
2026-07-04 13:50:11 +0200

    Add files via upload

---
+++ hw 01.07.26.py
@@ -0,0 +1,27 @@
+# 1.
+# counter = -1
+# while counter < 10:
+#     counter += 1
+#     print(counter)
+
+# 2.
+# c = -1
+# while c < 19 :
+#     c +=2
+#     print(c)
+
+# 3.
+# c = 21
+# while c > 10:
+#     c -= 1
+#     print(c)
+
+# # 4.
+# c = 5
+# while 0 < c <= 5:
+#     c -= 1
+#     print('Python')
+
+# 5.
+# n = int(input())
+# print(len(str(n)))
