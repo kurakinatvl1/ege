15161ec1ee1c5d2dd2e4f2ee4b7e80503a6ab654
kurakinatvl1 <kurakinatvl1@gmail.com>
2026-07-07 19:16:53 +0200

    Add files via upload

--- 4th 27.06.26.py
+++ 4th 27.06.26.py
@@ -53,20 +53,93 @@
 # n2 = 2
 # match n2:
 #     case 4,6,9,11:
-#         if 1<n1<=30:
+#         if 1<=n1<=30:
 #           print('correct')
 #         else:
 #             print('error')
 #     case 1, 3 , 5 , 7 , 8 , 10 ,12:
-#         if 1 < n1 <= 30:
+#         if 1 <= n1 <= 30:
 #          print('correct')
 #         else:
 #          print('error')
 #     case 2:
-#         if 1 < n1 <= 30:
+#         if 1 <= n1 <= 30:
 #          print('correct')
 #         else:
 #          print('error')
 #     case _:
 #          print('error')
 
+
+# x = int(input())
+# n = int(input())
+# if n == 50 or n == 100 or n == 200:
+#  if x>n:
+#     print('insufficient funds')
+#  else:
+#      change = n-x
+#      z10 = change//10
+#      z5 = (change%10)//5
+#      z2 = (change%10)- z5*5 )//2
+#      z1 = (change%10)- z5*5 )%2
+#
+#      print(z10, '- tens,', z5, '- пятерок,', z2, '- двоек,', z1, 'единиц' )
+# else :
+#      print('error')
+
+#
+# r = int(input())
+# t = str(input())
+# if 1<=r<= 3 :
+#     if t == 'day':
+#      print('150r')
+#     else:
+#      print(150*1.3)
+# else:
+#     if t == 'day':
+#      print((r-3)*20)
+#     else:
+#      print((r-3)*20*1.3)
+
+
+
+# a = 1
+# # print(f'dssss {a+a}gggg') #f строки
+# s = '{1}dddd{2}yy{0}'.format(' its ',' not ',' bad ')
+# s = '{city}ddddyy'.format(city=' its ')
+# print(s)
+
+
+# h = int(input())
+# m = int(input())
+# s = int(input())
+# if 0<=h<=23 and 0<=m<=59 and 0<=s<=59:
+#  print(f'{h}:{m}:{s} ')
+# else:
+#  print('error')
+
+
+
+# a1 = 162
+# a2 = 168
+# a3 = 1
+# a4 = 10
+# cnt = 0
+# if 0<= a1 <= 255:
+#     if 0<= a2 <= 255:
+#         if 0<= a3 <= 255:
+#             if 0<= a4 <= 255:
+#                 cnt += 1
+# if cnt == 0:
+#     print('invalid')
+# else:
+#   if 1<= a1 <= 126:
+#     print('class a')
+#   elif 128<= a1 <= 191:
+#     print('class b')
+#   elif 192 <= a1 <= 223:
+#     print('class c')
+#   elif a1 == 127 or a1 == 0 or a1 >=223:
+#     print('sluzebnyi')
+# ip = f'{a1}.{a2}.{a3}.{a4}'
+# if (a1 ==  10 and 0<= a2 <=255 ...
