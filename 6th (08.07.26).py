95e83a265a1615c10318ec3e21c802483096192a
kurakinatvl1 <kurakinatvl1@gmail.com>
2026-07-08 17:48:07 +0200

    Add files via upload

---
+++ 6th (08.07.26).py
@@ -0,0 +1,107 @@
+# while условие :
+#     ткло цтула
+
+    # break
+
+    # итерируем - list tuple dict set str
+
+
+    # for element in posledovatelnost
+    #  body of cicle
+# s = [1,2,3,4,5,6,7,8,9,10]
+# d = 'hello world'
+# for element in d:
+#      print(element)
+
+# s = range(start,stop,step)- начало ,конец(не включительно), интервал ежду элементами последовательности
+# s = list(range(1,101))
+# for i in s:
+#  print(i)
+
+
+# for i in range(100):
+#     print(i)
+
+# s = 'hello world'
+# for i in range(6,11):
+#     print(s[i])
+
+# s = list(range(0,101,2))
+# print(s)
+
+# for i in range(20,11,-2):
+#     print(i)
+
+# for i in range(1,3):
+#     print('vne',i)
+#     for j in range (1,3):
+#         print('vnu',j)
+#     print()
+
+# for i in range(1,5):
+#     for j in range(1,10):
+#        print(i,'*',j,'=',i*j)
+#     print()
+
+# n = 1
+# n2 = 1
+# while n < 10 and n2<10:
+#     print(n,'*',n2,'=',n*n2)
+#     n += 1
+#     n2 += 1
+#     print()
+
+
+# i = 0
+# while i < 10:
+#     j  = 0
+#     i += 1
+#     while j < 10:
+#         j += 1
+#         print(i,'*',j,'=',i*j)
+
+# i = 0
+# for  i in range (6):
+#     j  = 0
+#     while j < 10:
+#         j += 1
+#         print(i,'*',j,'=',i*j)
+#     print()
+
+
+
+
+
+
+
+# for i in range(5,0,-1):
+#     print('*'* i)
+#
+# for i in range (5,0,-1):
+#     for j in range (i):
+#      print('*',end = '')
+#     print()
+
+# print ('hello','world', sep='',end ='')
+# print ('123')
+
+
+# for i in range(1,10,2):
+#     print(' ',i*"#")...
+
+# n = 5
+# for i in range(n):
+#         print(' '*(n-i-1),'#'*(2*i+1),sep='')
+
+# n = 5
+# for i in range(n):
+#  for j in range(n-i-1):
+#     print(' ',end='')
+#  for j in range((2*i+1)):
+#    print('#',end = '')
+#  print()
+
+
+
+
+
