92101689ab7a6f6c2abb22e15a7e72048fbeefd0
kurakinatvl1 <kurakinatvl1@gmail.com>
2026-07-11 14:17:50 +0200

    Add files via upload

---
+++ hw6 (08.07.26).py
@@ -0,0 +1,68 @@
+
+# 1.
+# *
+# **
+# ***
+# ****
+# ***
+# **
+# *
+
+# z = '*'
+# n = 8
+# for x in range(n):
+#         if x<=4:
+#             print(z*x)
+#         else:
+#             print(z*(n-x))
+
+
+
+# 2.
+# @@@@@
+# @@@@@
+# @@@@@
+# @@@@@
+# @@@@@
+
+# z = '@@@@@'
+# for i in range(5):
+#     print(z)
+
+
+# 3.
+# ***
+# * * *
+# ** **
+# * * *
+#  ***
+#//////
+# n = 3
+# z = '***'
+# a = '* * *'
+# b = '** **'
+# for i in range(n):
+#     if i==1:
+#         print(b)
+#     elif i<1:
+#         print(z)
+#         print(a)
+#     else:
+#         print(a)
+#         print('',z)
+
+
+# 4.
+
+#   +
+#   +
+# +++++
+#   +
+#   +
+
+# z = '+'
+# for i in range(5):
+#     if i == 2:
+#         print(z*5)
+#     else:
+#         print(' ',z)
